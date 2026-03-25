b3dae477e567f6e270ce5a34e846ccb340121e8c
yscof <dexter1541@naver.com>
2026-03-25 11:00:05 +0900

    문제008 '좋은 수' 구하기

--- src/DoItAlgorithmCodingTest/Chapter2/Section04/Unit03/Problem008.java
+++ src/DoItAlgorithmCodingTest/Chapter2/Section04/Unit03/Problem008.java
@@ -1,9 +1,50 @@
 package DoItAlgorithmCodingTest.Chapter2.Section04.Unit03;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
 // 문제 008 '좋은 수 구하기'
 // 백준 1253번
 public class Problem008 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        // N 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        // N개의 정수 배열 입력받기
+        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        // 배열 정렬
+        Arrays.sort(A);
+        int answer = 0;
+        // 투 포인터로 탐색
+        for (int i = 0; i < N; i++) {
+            int startPointer = 0;
+            int endPointer = N - 1;
+            while (startPointer < endPointer) {
+                if (A[startPointer] + A[endPointer] == A[i]) {
+                    if (startPointer != i && endPointer != i) {
+                        answer++;
+                        break;
+                    } else if (startPointer == i) {
+                        startPointer++;
+                    } else if (endPointer == i) {
+                        endPointer--;
+                    }
+                } else if (A[startPointer] + A[endPointer] > A[i]) {
+                    endPointer--;
+                } else {
+                    startPointer++;
+                }
+            }
+        }
+        System.out.println(answer);
+        br.close();
     }
 }
---
+++ src/DoItAlgorithmCodingTest/Chapter2/Section04/Unit03/Problem008_book.java
@@ -0,0 +1,46 @@
+package DoItAlgorithmCodingTest.Chapter2.Section04.Unit03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 문제 008 '좋은 수 구하기'
+// 백준 1253번
+public class Problem008_book {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int Result = 0;
+        long A[] = new long[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(A);
+        for (int k = 0; k < N; k++) {
+            long find = A[k];
+            int i = 0;
+            int j = N - 1;
+            while (i < j) {
+                if (A[i] + A[j] == find) {
+                    if (i != k && j != k) {
+                        Result++;
+                        break;
+                    } else if (i == k) {
+                        i++;
+                    } else if (j == k) {
+                        j--;
+                    }
+                } else if (A[i] + A[j] < find) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+        System.out.println(Result);
+        bf.close();
+    }
+}
