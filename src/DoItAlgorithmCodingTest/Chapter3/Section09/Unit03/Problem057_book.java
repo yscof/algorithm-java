2abb59532382422e3d6acd3455dfba8ce43ab5b3
yscof <dexter1541@naver.com>
2026-04-02 15:53:11 +0900

    문제 057 게임 개발하기

--- src/DoItAlgorithmCodingTest/Chapter3/Section09/Unit03/Problem057_book.java
+++ src/DoItAlgorithmCodingTest/Chapter3/Section09/Unit03/Problem057_book.java
@@ -1,20 +1,21 @@
 package DoItAlgorithmCodingTest.Chapter3.Section09.Unit03;
 
 /*
-문제057 게임 개발하기 / 백준 1516번 (꼴드 3)
+문제057 게임 개발하기 / 백준 1516번 (골드 3)
 
 N(건물 종류 수), A(데이터 저장 인접 리스트)
 건물의 개수만큼 인접 리스트 초기화하기
+진입 차수 배열 초기화하기
 자기 자신을 짓는 데 걸리는 시간 저장 배열 초기화하기
 for (건물의 개수) {
     인접 리스트 데이터 저장하기
-    진입 차수 배열 초기 데이터 저장하기
+    잔입 차수 배열 초기 데이터 저장하기
     자기 자신을 짓는 데 걸리는 시간 배열 데이터 저장하기
 }
 // 위상 정렬 수행하기
 큐 생성하기
 for (건물의 개수) {
-    진입 차수 배열의 0인 건물(노드)을 큐에 삽입하기
+    진입 차수 배열의 값이 0인 건물(노드)을 큐에 삽입하기
 }
 while (큐가 빌 때까지) {
     현재 노드 = 큐에서 데이터 poll
@@ -33,9 +34,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Problem057_book {
     public static void main(String[] args) throws IOException {
@@ -45,18 +46,18 @@ public class Problem057_book {
         for (int i = 0; i <= N; i++) {
             A.add(new ArrayList<>());
         }
-        int[] indegree = new int[N + 1];        // 진입 차수 배열
-        int[] selfBuild = new int[N + 1];       // 자기 자신을 짓는 데 걸리는 시간
+        int[] indegree = new int[N + 1];            // 진입 차수 배열
+        int[] selfBuild = new int[N + 1];           // 자기 자신을 짓는 데 걸리는 시간
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            selfBuild[i] = Integer.parseInt(st.nextToken());        // 자신의 생산 시간 저장하기
-            while (true) {                                          // 인접 리스트 데이터 저장하기
+            selfBuild[i] = Integer.parseInt(st.nextToken());
+            while (true) {
                 int preTemp = Integer.parseInt(st.nextToken());
                 if (preTemp == -1) {
                     break;
                 }
                 A.get(preTemp).add(i);
-                indegree[i]++;
+                indegree[i]++;                  // 진입 차수 배열 데이터 저장하기
             }
         }
         // 위상 정렬
@@ -71,7 +72,6 @@ public class Problem057_book {
             int now = queue.poll();
             for (int next : A.get(now)) {
                 indegree[next]--;
-                // 시간 업데이트하기
                 result[next] = Math.max(result[next], result[now] + selfBuild[now]);
                 if (indegree[next] == 0) {
                     queue.offer(next);
@@ -79,7 +79,7 @@ public class Problem057_book {
             }
         }
         for (int i = 1; i <= N; i++) {
-            System.out.println(result[i] + selfBuild[i]);
+            System.out.println(selfBuild[i] + result[i]);
         }
     }
 }
