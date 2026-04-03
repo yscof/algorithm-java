bf0241944f87335f34b572b047d31cab3fb16e0d
yscof <dexter1541@naver.com>
2026-04-03 14:16:36 +0900

    문제089 이친수 구하기

---
+++ src/DoItAlgorithmCodingTest/Chapter3/Section12/Problem089_book.java
@@ -0,0 +1,35 @@
+package DoItAlgorithmCodingTest.Chapter3.Section12;
+
+/*
+문제089 이친수 구하기 / 백준 2193번 (실버 3)
+
+D(점화식 배열)
+D[i][0]: 길이 i에서 끝이 0으로 끝나는 이친수의 개수
+D[i][1]: 길이 i에서 끝이 1로 끝나는 이친수의 개수
+N(자릿수)
+D 배열 초기화
+D[1][1] = 1     // 1은 이친수
+D[1][0] = 0     // 이친수는 0으로 시작하지 않기 때문에 1자리 0으로 끝나는 이친수는 없음
+for (i -> 2 ~ N) {
+    i번째 0으로 끝나는 개수 = i - 1에서 0으로 끝나는 개수 + i - 1에서 1로 끝나는 개수
+    i번째 1로 끝나는 개수 = i - 1에서 0으로 끝나는 개수
+}
+N번째에서 0으로 끝나는 개수 + N번째에서 1로 끝나는 개수 출력하기
+*/
+
+import java.util.Scanner;
+
+public class Problem089_book {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long D[][] = new long[N + 1][2];
+        D[1][1] = 1;        // 1자리 이친수는 1 한 가지
+        D[1][0] = 0;
+        for (int i = 2; i <= N; i++) {
+            D[i][0] = D[i - 1][1] + D[i - 1][0];
+            D[i][1] = D[i - 1][0];
+        }
+        System.out.println(D[N][0] + D[N][1]);
+    }
+}
